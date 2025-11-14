3509faf3496606e9d7a9c3bf50dedac284bff5ae
Nathan Kirby <natekirbz@gmail.com>
2025-11-14 14:23:52 -0500

    made viewer stage its own class

--- Final/src/gui/Tracks.java
+++ Final/src/gui/Tracks.java
@@ -1,65 +1,44 @@
 package gui;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.geom.Point2D;
 
 import io.ResourceFinder;
 import visual.dynamic.described.RuleBasedSprite;
-import visual.dynamic.described.SampledSprite;
 import visual.dynamic.described.Stage;
-import visual.statik.described.AggregateContent;
-import visual.statik.sampled.Content;
 import visual.statik.sampled.ContentFactory;
-import visual.dynamic.described.*;
+import visual.statik.sampled.TransformableContent;
+import visual.VisualizationView;
+
 import resources.Marker;
-import java.awt.Shape;
 
 
-public class Tracks extends Stage implements KeyListener{
-	private RuleBasedSprite player;
-    private int lane = 1; // 0 = left, 1 = middle, 2 = right
-    private int[] laneX = {0, 150, 300}; // x-coordinates for each lane
-    private ResourceFinder jarFinder;
+public class Tracks extends Stage{
 
     public Tracks() {
-        super(60);
-        addKeyListener(this);
+        super(50);
+        ResourceFinder finder = ResourceFinder.createInstance(Marker.class);
+		ContentFactory tcFactory = new ContentFactory(finder);
+		int stageWidth = 800;
+		int stageHeight = 450;
+        VisualizationView stageView = getView();
+        stageView.setBounds(0, 0, stageWidth, stageHeight);
+		TransformableContent content = tcFactory.createContent("shark.png", 4, false);
+		Player player = new Player(content, (double)stageWidth, (double)stageHeight);
+		add(player);
+		addKeyListener(player);
+        
+        for(int i = 0; i < 2; ++i) {
+        	TransformableContent goldContent = tcFactory.createContent("balloon-gold.png", 4, false);
+			goldContent.setScale(0.1, 0.1);
+			Enemy gold = new Enemy(goldContent, (double)stageWidth, (double)stageHeight);
+			gold.addAntagonist(player);
+			add(gold);
+        	// TransformableContent purpleContent = tcFactory.createContent("bern.png", 4, false);
+			// Enemy purple = new Enemy(purpleContent, (double)stageWidth, (double)stageHeight);
+			// purple.addAntagonist(cupola);
+			// stage.add(purple);
+		}
 
-        jarFinder = ResourceFinder.createInstance(new Marker());
-        ContentFactory contentFactory = new ContentFactory(jarFinder);
-        
-        // Player setup
-        
-        Content logo = contentFactory.createContent("bern.png", 3);
-        player = new RuleBasedSprite(logo);
-        
-        player.addKeyTime(1, new Point2D.Double(0.0, 0.0), 0.0, 1.0, logo);
-        player.addKeyTime(10, new Point2D.Double(0.0, 0.0), 0.0, 1.0, null);
-
-
-        player.setLocation(laneX[lane], 300);
-        add(player);
-        
-        start();
-        setRestartTime(500);
     }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_LEFT && lane > 0) {
-            lane--;
-        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && lane < 2) {
-            lane++;
-        }
-        player.setLocation(laneX[lane], 500);
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {}
-    @Override
-    public void keyTyped(KeyEvent e) {}
+    
 
 }
--- Final/src/gui/Viewer.java
+++ Final/src/gui/Viewer.java
@@ -1,30 +1,24 @@
 package gui;
 
-import java.awt.Color;
-import java.awt.Toolkit;
 import java.io.IOException;
-
 import javax.swing.JPanel;
 
-import java.awt.Image;
-
 import app.JApplication;
+ 
 import visual.VisualizationView;
 import visual.dynamic.described.Stage;
-import visual.statik.sampled.ContentFactory;
-import gui.*;
-import io.ResourceFinder;
-import resources.Marker;
-
-import visual.statik.sampled.TransformableContent;
 
 public class Viewer extends JApplication
 {
 	public static final int HEIGHT = 450; //divisible by 3 150px tracks
 	public static final int WIDTH = 800;
+	public Stage stage;
 
 	public Viewer(final String[] args) {
 		super(WIDTH, HEIGHT);
+		
+		this.stage = new Tracks();
+
 	}
 	
 	public static void main(final String[] args) throws IOException
@@ -35,37 +29,11 @@ public class Viewer extends JApplication
 
 	@Override
 	public void init() {
-		ResourceFinder finder = ResourceFinder.createInstance(Marker.class);
-		ContentFactory tcFactory = new ContentFactory(finder);
-		int stageWidth = 800;
-		int stageHeight = 450;
-		Stage stage = new Stage(50);
-		VisualizationView stageView = stage.getView();
-		// Toolkit toolkit = stageView.getToolkit();
-		// ImageFactory imageFactory = new ImageFactory(finder);
-		// TransformableContent content = tcFactory.createContent("roof.png", 3, false);
-		// stage.add(content);
-		stageView.setBounds(0, 0, stageWidth, stageHeight);
-		TransformableContent content = tcFactory.createContent("cupola.png", 4, false);
-		Player player = new Player(content, (double)stageWidth, (double)stageHeight);
-		stage.add(player);
-		stage.addKeyListener(player);
+		VisualizationView view = stage.getView();
+    	JPanel cp = (JPanel) getContentPane();
+    	cp.add(view);
 
-      	for(int i = 0; i < 4; ++i) {
-        	TransformableContent goldContent = tcFactory.createContent("balloon-gold.png", 4, false);
-			Enemy gold = new Enemy(goldContent, (double)stageWidth, (double)stageHeight);
-			gold.addAntagonist(player);
-			stage.add(gold);
-        	// TransformableContent purpleContent = tcFactory.createContent("bern.png", 4, false);
-			// Enemy purple = new Enemy(purpleContent, (double)stageWidth, (double)stageHeight);
-			// purple.addAntagonist(cupola);
-			// stage.add(purple);
-		}
-
-		JPanel contentPane = (JPanel)this.getContentPane();
-		contentPane.add(stageView);
-		stage.start();
-		
+    	stage.start();
 	}
 
 
