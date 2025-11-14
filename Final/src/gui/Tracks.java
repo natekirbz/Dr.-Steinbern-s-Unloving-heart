1b080a23ed0584bcc3bf3f5319a5f4ada130e323
Nathan Kirby <natekirbz@gmail.com>
2025-11-14 14:27:06 -0500

    comments for tracks file

--- Final/src/gui/Tracks.java
+++ Final/src/gui/Tracks.java
@@ -1,8 +1,6 @@
 package gui;
 
-
 import io.ResourceFinder;
-import visual.dynamic.described.RuleBasedSprite;
 import visual.dynamic.described.Stage;
 import visual.statik.sampled.ContentFactory;
 import visual.statik.sampled.TransformableContent;
@@ -15,23 +13,31 @@ public class Tracks extends Stage{
 
     public Tracks() {
         super(50);
-        ResourceFinder finder = ResourceFinder.createInstance(Marker.class);
-		ContentFactory tcFactory = new ContentFactory(finder);
 		int stageWidth = 800;
 		int stageHeight = 450;
+
+        //build visView
         VisualizationView stageView = getView();
         stageView.setBounds(0, 0, stageWidth, stageHeight);
+
+        // resource finder to create player
+        ResourceFinder finder = ResourceFinder.createInstance(Marker.class);
+		ContentFactory tcFactory = new ContentFactory(finder);
 		TransformableContent content = tcFactory.createContent("shark.png", 4, false);
+
 		Player player = new Player(content, (double)stageWidth, (double)stageHeight);
 		add(player);
 		addKeyListener(player);
         
         for(int i = 0; i < 2; ++i) {
+
+            // create 2 enemies 
         	TransformableContent goldContent = tcFactory.createContent("balloon-gold.png", 4, false);
 			goldContent.setScale(0.1, 0.1);
 			Enemy gold = new Enemy(goldContent, (double)stageWidth, (double)stageHeight);
 			gold.addAntagonist(player);
 			add(gold);
+            
         	// TransformableContent purpleContent = tcFactory.createContent("bern.png", 4, false);
 			// Enemy purple = new Enemy(purpleContent, (double)stageWidth, (double)stageHeight);
 			// purple.addAntagonist(cupola);
